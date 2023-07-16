c7fe82bc8a3c440f86c49c346d37af8664dc97fe
Catherine Brower <catherinemb42@gmail.com>
2023-07-16 00:31:35 -0400

    modal work

--- src/gui/Window.tsx
+++ src/gui/Window.tsx
@@ -1,10 +1,23 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Paper, Typography } from "@mui/material";
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grow, IconButton, Paper, Slide, Typography } from "@mui/material";
 import React from "react";
 import Draggable from "react-draggable";
 import { IWindow } from "./GUITypes";
 import { useAppDispatch } from "../Hooks";
 import { closeObjectWindow } from "./GUISlice";
 import ObjectEditor from "./windows/ObjectEditor";
+import CloseIcon from "@mui/icons-material/CloseRounded";
+import FullscreenIcon from "@mui/icons-material/FullscreenRounded";
+import MinimizeIcon from "@mui/icons-material/MinimizeRounded";
+import { TransitionProps } from "@mui/material/transitions";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>,
+) {
+  return <Grow in={true} ref={ref} {...props} />;
+});
 
 interface IProps {};
 interface IState {
@@ -22,12 +35,13 @@ export class PaperComponent extends React.Component<IProps, IState> {
                 handle="#draggable-dialog-title"
                 cancel={'[class*="MuiDialogContent-root"]'}
             >
-                <Paper {...this.props} />
+              <Paper {...this.props} />
             </Draggable>
         )
     }
 }
 
+// TODO this componet dissapears on close, this breaks the outgoing transition
 export default function Window(props: IWindow) {
   const dispatch = useAppDispatch();
   const window = props;
@@ -40,6 +54,10 @@ export default function Window(props: IWindow) {
     dispatch(closeObjectWindow(window.id));
   }
 
+  function handleSave(event: any, reason: any) {
+
+  }
+
   function getDialogContent() {
     if(window.type === "object") {
       return (<ObjectEditor {...window.data} />)
@@ -49,30 +67,35 @@ export default function Window(props: IWindow) {
   }
 
   return (
-    <div>
-      <Dialog
-        open={true}
-        onClose={handleClose}
-        PaperComponent={PaperComponent}
-        aria-labelledby="draggable-dialog-title"
-        hideBackdrop={true}
-        disableEnforceFocus={true}
-        
-      >
-        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
-          {window.name}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            {getDialogContent()}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button autoFocus onClick={() => {handleClose(null, "closeBtn")}}>
-            Close
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </div>
+    <Dialog
+      open={true}
+      TransitionComponent={Transition}
+      onClose={handleClose}
+      PaperComponent={PaperComponent}
+      aria-labelledby="draggable-dialog-title"
+      hideBackdrop={true}
+      disableEnforceFocus={true}
+      className="dialog"
+    >
+      <DialogTitle style={{ cursor: 'move', height: "2em", display: "flex", padding: "10px 0px 25px 15px" }} id="draggable-dialog-title" className="dialogHeader">
+        <Typography variant="subtitle2" sx={{flexGrow: 1}}>{window.name}</Typography>
+        <IconButton onClick={() => {handleClose(null, "closeBtn")}}><CloseIcon fontSize="small"/></IconButton>
+        <IconButton><FullscreenIcon fontSize="small"/></IconButton>
+        <IconButton><MinimizeIcon fontSize="small"/></IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText>
+          {getDialogContent()}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={() => {handleSave(null, "saveBtn")}}>
+          Save
+        </Button>
+        <Button autoFocus onClick={() => {handleClose(null, "closeBtn")}}>
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 }
--- src/gui/windows/ObjectEditor.tsx
+++ src/gui/windows/ObjectEditor.tsx
@@ -1,6 +1,127 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Stack, TextField, Typography } from "@mui/material";
+import { Accordion, AccordionDetails, AccordionSummary, Box, Stack, Tab, Tabs, TextField, Typography, styled } from "@mui/material";
 import { IProgramEuler, IProgramObject } from "../../code/Program";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { useState } from "react";
+
+const modalWidth = 500;
+const textFeildVariant = "standard";
+
+const AntTabs = styled(Tabs)({
+    borderBottom: '1px solid #e8e8e8',
+    '& .MuiTabs-indicator': {
+      backgroundColor: '#1890ff',
+    },
+  });
+  
+  const AntTab = styled((props: StyledTabProps) => <Tab disableRipple {...props} />)(
+    ({ theme }) => ({
+      textTransform: 'none',
+      minWidth: 0,
+      [theme.breakpoints.up('sm')]: {
+        minWidth: 0,
+      },
+      fontWeight: theme.typography.fontWeightRegular,
+      marginRight: theme.spacing(1),
+      color: 'rgba(0, 0, 0, 0.85)',
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+      '&:hover': {
+        color: '#40a9ff',
+        opacity: 1,
+      },
+      '&.Mui-selected': {
+        color: '#1890ff',
+        fontWeight: theme.typography.fontWeightMedium,
+      },
+      '&.Mui-focusVisible': {
+        backgroundColor: '#d1eaff',
+      },
+    }),
+  );
+  
+  interface StyledTabsProps {
+    children?: React.ReactNode;
+    value: number;
+    onChange: (event: React.SyntheticEvent, newValue: number) => void;
+  }
+  
+  const StyledTabs = styled((props: StyledTabsProps) => (
+    <Tabs
+      {...props}
+      TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+    />
+  ))({
+    '& .MuiTabs-indicator': {
+      display: 'flex',
+      justifyContent: 'center',
+      backgroundColor: 'transparent',
+    },
+    '& .MuiTabs-indicatorSpan': {
+      maxWidth: 40,
+      width: '100%',
+      backgroundColor: '#635ee7',
+    },
+  });
+  
+  interface StyledTabProps {
+    label: string;
+  }
+  
+  const StyledTab = styled((props: StyledTabProps) => (
+    <Tab disableRipple {...props} />
+  ))(({ theme }) => ({
+    textTransform: 'none',
+    fontWeight: theme.typography.fontWeightRegular,
+    fontSize: theme.typography.pxToRem(15),
+    marginRight: theme.spacing(1),
+    color: 'rgba(255, 255, 255, 0.7)',
+    '&.Mui-selected': {
+      color: '#fff',
+    },
+    '&.Mui-focusVisible': {
+      backgroundColor: 'rgba(100, 95, 228, 0.32)',
+    },
+  }));
+
+
+
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`vertical-tabpanel-${index}`}
+            aria-labelledby={`vertical-tab-${index}`}
+            {...other}
+        >
+
+        {value === index && (
+            <Box sx={{ pl: 3, width: `${modalWidth}px`}}>
+                {children}
+            </Box>
+        )}
+        </div>
+    );
+}
 
 function editableEuler(props: IProgramEuler | undefined, name: string) {
     return (
@@ -14,22 +135,21 @@ function editableEuler(props: IProgramEuler | undefined, name: string) {
             </AccordionSummary>
             <AccordionDetails>
                 <Stack direction="column" spacing={2}>
-                    <TextField id={`${name}_x`} label="X" defaultValue={props?.x}/>
-                    <TextField id={`${name}_y`} label="Y" defaultValue={props?.y}/>
-                    <TextField id={`${name}_z`} label="Z" defaultValue={props?.z}/>
+                    <TextField variant={textFeildVariant} id={`${name}_x`} label="X" defaultValue={props?.x}/>
+                    <TextField variant={textFeildVariant} id={`${name}_y`} label="Y" defaultValue={props?.y}/>
+                    <TextField variant={textFeildVariant} id={`${name}_z`} label="Z" defaultValue={props?.z}/>
                 </Stack>
             </AccordionDetails>
         </Accordion>
     )
 }
 
-export default function ObjectEditor(props: IProgramObject) {
-    // const objectData = findObjectById(props.id);
-
+// TODO check for / create a new interface to specify props
+// events should have the exact same type as this
+// doing it this way, I can avoid writing a specialized editor for each event
+function EditorSection(props: any) {
     return (
-        <Stack direction="column" spacing={3} py={1}>
-            <TextField id="name" label="Name" defaultValue={props.name}/>
-
+        <Stack direction="column" spacing={1} py={1}>
             <Accordion>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
@@ -40,10 +160,10 @@ export default function ObjectEditor(props: IProgramObject) {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Stack direction="column" spacing={2}>
-                        <TextField id="color_r" label="R" defaultValue={props.color?.r}/>
-                        <TextField id="color_g" label="G" defaultValue={props.color?.g}/>
-                        <TextField id="color_b" label="B" defaultValue={props.color?.b}/>
-                        <TextField id="color_a" label="A" defaultValue={props.color?.a}/>
+                        <TextField variant={textFeildVariant} id="color_r" label="R" defaultValue={props.color?.r}/>
+                        <TextField variant={textFeildVariant} id="color_g" label="G" defaultValue={props.color?.g}/>
+                        <TextField variant={textFeildVariant} id="color_b" label="B" defaultValue={props.color?.b}/>
+                        <TextField variant={textFeildVariant} id="color_a" label="A" defaultValue={props.color?.a}/>
                     </Stack>
                 </AccordionDetails>
             </Accordion>
@@ -51,7 +171,37 @@ export default function ObjectEditor(props: IProgramObject) {
             {editableEuler(props.rotation, "rotation")}
             {editableEuler(props.translation, "translation")}
             {editableEuler(props.scale, "scale")}
-
         </Stack>
+    )
+}
+
+export default function ObjectEditor(props: IProgramObject) {
+    const [tabIndex, setTabIndex] = useState(0);
+
+    const handleTabChange = (event: React.SyntheticEvent, newIndex: number) => {
+        setTabIndex(newIndex);
+    }
+
+    return (
+        <Box
+            sx={{flexGrow: 1, bgcolor: 'background.paper', display: 'flex', height: 224}}
+        >
+            <Tabs
+                value={tabIndex}
+                onChange={handleTabChange}
+                orientation="vertical"
+            >
+                <Tab label="Properties" value={0} />
+                <Tab label="Create" value={1}/>
+                <Tab label="Click" value={2}/>
+            </Tabs>
+            <TabPanel value={tabIndex} index={0}>
+                <TextField variant={textFeildVariant} id="name" label="Name" defaultValue={props.name}/>
+            </TabPanel>
+            <TabPanel value={tabIndex} index={1}>
+                <EditorSection {...props} />
+            </TabPanel>
+            <TabPanel value={tabIndex} index={2}>YAAAK</TabPanel>
+        </Box>
     )
 }
