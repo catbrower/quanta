3d9f22d5da4b4ec49280a0097630aa6e139fa055
Catherine Brower <catherinemb42@gmail.com>
2023-07-20 23:34:51 -0400

    intial compiler work complete

--- src/gui/ProgramEditor.tsx
+++ src/gui/ProgramEditor.tsx
@@ -2,34 +2,13 @@ import TreeView from "@mui/lab/TreeView";
 import TreeItem from "@mui/lab/TreeItem";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import { Collapse, Container } from "@mui/material";
-import { TransitionProps } from "@mui/material/transitions";
-import { useSpring, animated } from '@react-spring/web';
+import { Container } from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../redux/Hooks";
 import { openObjectWindow } from "../redux/GUISlice";
 
-function TransitionComponent(props: TransitionProps) {
-    const style = useSpring({
-      from: {
-        opacity: 0,
-        transform: 'translate3d(20px, 0, 0)',
-      },
-      to: {
-        opacity: props.in ? 1 : 0,
-        transform: `translate3d(${props.in ? 0 : 20}px, 0, 0)`,
-      },
-    });
-  
-    return (
-      <animated.div style={style}>
-        <Collapse {...props} />
-      </animated.div>
-    );
-  }
-
 export default function ProgramEditor() {
   const dispatch = useAppDispatch();
-  // const objects = useAppSelector(state => state.code.objects);
+  const objects = useAppSelector(state => state.code.program.objects);
 
   return (
       <Container sx={{py: "15px"}}>
@@ -40,9 +19,9 @@ export default function ProgramEditor() {
               sx={{ height: 240, flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
               >
               <TreeItem nodeId="1" label="Objects">
-                  {/* {objects.map((object) => (
+                  {objects.map((object) => (
                       <TreeItem nodeId="0" label={object.name} onDoubleClick={() => dispatch(openObjectWindow(object))}/>
-                  ))} */}
+                  ))}
               </TreeItem>
               <TreeItem nodeId="5" label="Scripts">
               </TreeItem>
